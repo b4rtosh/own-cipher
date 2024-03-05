d979b4a38eef2b4a0659ef5b398750365bb354b4
Bartosh <b4rtosh@proton.me>
2024-03-05 18:43:19 +0100

    Basic front. Working encoding.

---
+++ cipher_script.js
@@ -0,0 +1,83 @@
+const button_encode = document.getElementById("encode")
+const button_decode = document.getElementById("decode")
+const message_area = document.getElementById("txt_to_code")
+
+button_encode.addEventListener("click", encodeClicked)
+button_decode.addEventListener("click", decodeClicked)
+
+
+function removeWhitespaces(message){
+    message = message.replace(/\s+/g, ' ');
+    if (message.slice(-1).match(/\s/)){
+        message = message.slice(0, -1);
+    }
+    return message;
+}
+
+
+function getBlocksLength(message){
+    let words = message.split(' ');
+    console.log(words);
+    return words.length;
+}
+
+function divideIntoBlocks(message, size){
+    let block_arr = [];
+    let block = '';
+    for (let letter of message){
+        block += letter;
+        if (block.length % size === 0){
+            block_arr.push(block);
+            block = '';
+        }
+    }
+    if (block.length !== 0){
+        block_arr.push(block);
+    }
+    return block_arr;
+}
+
+function prepareMessage(message){
+    message = removeWhitespaces(message);
+    let block_size = message.split(' ').length;
+    let block_list = divideIntoBlocks(message, block_size);
+    return block_list;
+}
+
+
+function encode(message){
+    let block_list = prepareMessage(message);
+    let result = '';
+    for (let word of block_list){
+        if (word.length % 2 === 0){
+            for (let i = 0; i < (word.length / 2); i++){
+                result += word[word.length - 1 - i];
+                result += word[i];
+            }
+        }
+        else{
+            for (let j = 0; j < (Math.floor(word.length / 2)); j++){
+                result += word[word.length - 1 - j];
+                result += word[j];
+            }
+            result += word[Math.floor(word.length / 2)]
+        }
+    }
+    return result;
+}
+
+
+
+function encodeClicked(){
+    let message = message_area.value;
+    let encoded = encode(message);
+    console.log(encoded);
+    message_area.value = encoded;
+}
+
+function decodeClicked(){
+    let message = message_area.value;
+    let decoded = encode(message);
+    console.log(decoded);
+    message_area.value = decoded;
+}
