d32a172d1dee21d626a2093c580c28cfa1adaf0a
Bartosh <b4rtosh@proton.me>
2024-03-06 23:37:16 +0100

    Add decoder. Change front. Add style file.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # own-cipher
 Simple cipher which allows encoding messages that can't be read easily.
+
+##
+Cipher uses amount of words to divide message into smaller blocks. Then it mixes chars in block: last char of block,
+then first char, up to the middle of block.
--- cipher_script.js
+++ cipher_script.js
@@ -6,78 +6,78 @@ button_encode.addEventListener("click", encodeClicked)
 button_decode.addEventListener("click", decodeClicked)
 
 
-function removeWhitespaces(message){
+function removeWhitespaces(message) {
     message = message.replace(/\s+/g, ' ');
-    if (message.slice(-1).match(/\s/)){
+    if (message.slice(-1).match(/\s/)) {
         message = message.slice(0, -1);
     }
     return message;
 }
 
 
-function getBlocksLength(message){
-    let words = message.split(' ');
-    console.log(words);
-    return words.length;
-}
-
-function divideIntoBlocks(message, size){
+function divideIntoBlocks(message, size) {
     let block_arr = [];
     let block = '';
-    for (let letter of message){
+    for (let letter of message) {
         block += letter;
-        if (block.length % size === 0){
+        if (block.length % size === 0) {
             block_arr.push(block);
             block = '';
         }
     }
-    if (block.length !== 0){
+    if (block.length !== 0) {
         block_arr.push(block);
     }
     return block_arr;
 }
 
-function prepareMessage(message){
+function prepareMessage(message) {
     message = removeWhitespaces(message);
-    let block_size = message.split(' ').length;
-    let block_list = divideIntoBlocks(message, block_size);
-    return block_list;
+    let block_size = message.match(/ /g).length;
+    return divideIntoBlocks(message, block_size);
 }
 
 
-function encode(message){
+function encode(message) {
     let block_list = prepareMessage(message);
     let result = '';
-    for (let word of block_list){
-        if (word.length % 2 === 0){
-            for (let i = 0; i < (word.length / 2); i++){
-                result += word[word.length - 1 - i];
-                result += word[i];
-            }
+    for (let word of block_list) {
+        for (let j = 0; j < (Math.floor(word.length / 2)); j++) {
+            result += word[word.length - 1 - j];
+            result += word[j];
         }
-        else{
-            for (let j = 0; j < (Math.floor(word.length / 2)); j++){
-                result += word[word.length - 1 - j];
-                result += word[j];
-            }
-            result += word[Math.floor(word.length / 2)]
+        if (word.length % 2 !== 0) {
+            result += word[Math.floor(word.length / 2)];
         }
     }
     return result;
 }
 
+function decode(message){
+    let block_list = prepareMessage(message);
+    let result = '';
+    for (let word of block_list){
+            for (let i = 1; i < word.length; i += 2){
+                result += word[i];
+            }
+            for (let j = word.length % 2 === 0 ? word.length - 2 : word.length -1; j >= 0; j -= 2) {
+                result += word[j];
+            }
+    }
+    return result;
+}
 
 
-function encodeClicked(){
+function encodeClicked() {
     let message = message_area.value;
     let encoded = encode(message);
     console.log(encoded);
     message_area.value = encoded;
 }
 
-function decodeClicked(){
+function decodeClicked() {
     let message = message_area.value;
-    let decoded = encode(message);
+    let decoded = decode(message);
     console.log(decoded);
     message_area.value = decoded;
 }
